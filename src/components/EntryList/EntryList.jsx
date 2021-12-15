17eafca45ddacf885bb22ec0a32ee259294aa115
Cristian Montes <cmontesmujica@gmail.com>
2021-12-14 18:12:25 -0700

    list of entries rendering

---
+++ src/components/EntryList/EntryList.jsx
@@ -0,0 +1,23 @@
+import { useEntries } from "../../context/EntryContext";
+import Entry from "../Entry/Entry";
+
+
+const EntryList = () => {
+    const { entries } = useEntries();
+    return(
+        <div>
+            <ul>
+                {entries.map((entry) => {
+                    return(
+                        <li key={`${entry.name}-${entry.message}`}>
+                            <Entry entry={entry} />
+                        </li>
+                    )
+                })}
+            </ul>
+        </div>
+    )
+}
+
+
+export default EntryList;
--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,16 +1,15 @@
 import React from "react";
+import EntryList from "../../components/EntryList/EntryList";
 
 import GuestBook from "../../components/GuestBook/GuestBook";
 import { EntryProvider } from "../../context/EntryContext";
 
 const Home = () => {
     return(
-        <>
         <EntryProvider>
             <GuestBook />
-            <h2> Entri list component </h2>
+            <EntryList />
         </EntryProvider>
-        </>
     )
 }
 
